70b2a590c92c39d02ae90f670e769b7a1b560a6c
Alex Dognini <alexlelecodog@outlook.com>
2025-03-24 15:39:32 -0300

    Data structure: adicionando imagens ao models e renderizando no store.html

--- store/models.py
+++ store/models.py
@@ -14,9 +14,19 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+    #method to render a image or an empty string
+    @property
+    def imageURL(self): 
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
