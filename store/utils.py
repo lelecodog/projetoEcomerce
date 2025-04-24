ead7b0a63bf37d3576fccd12653af9d4290f84a9
Alex Dognini <alexlelecodog@outlook.com>
2025-04-24 16:17:24 -0300

    Created Readme.md and coments on viwes.py and utils.py

--- store/utils.py
+++ store/utils.py
@@ -1,29 +1,34 @@
 import json
 from .models import *
 
+# Função para lidar com o carrinho de compras baseado em cookies (usuários não autenticados)
 def cookieCart(request):
-    # Create empty cart for non-logged in users
+    # Tenta carregar o carrinho dos cookies; se não existir, cria um carrinho vazio
     try:
         cart = json.loads(request.COOKIES['cart'])
     except:
         cart = {} 
-        print('CART:', cart)
     
+    # Inicializa os itens, o pedido e o total de itens no carrinho
     items = []
     order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
     cartItems = order['get_cart_items']
 
+    # Itera sobre os itens no carrinho
     for i in cart:
-        # Try block to prevent items in cart that may have been removed
+        # Bloco try para evitar erros caso um produto tenha sido removido do banco de dados
         try:
             cartItems += cart[i]['quantity']
 
+            # Obtém o produto do banco de dados
             product = Product.objects.get(id=i)
             total = (product.price * cart[i]['quantity'])
 
+            # Atualiza o total do pedido e o número de itens no carrinho
             order['get_cart_total'] += total
             order['get_cart_items'] += cart[i]['quantity']
 
+            # Cria um dicionário representando o item no carrinho
             item = {
                 'product': {
                     'id': product.id,
@@ -36,6 +41,7 @@ def cookieCart(request):
                 }
             items.append(item)
 
+            # Verifica se o produto não é digital para definir a necessidade de envio
             if product.digital == False:
                 order ['shipping'] = True
         except:
@@ -43,13 +49,16 @@ def cookieCart(request):
 
     return {'cartItems': cartItems, 'order': order, 'items': items}
 
+# Função para obter os dados do carrinho, seja para usuários autenticados ou visitantes
 def cartData(request):
     if request.user.is_authenticated:
+        # Para usuários autenticados, obtém o cliente e o pedido associado
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        # Para visitantes, utiliza os dados do carrinho baseado em cookies
         cookieData = cookieCart(request)
         cartItems = cookieData['cartItems']
         order = cookieData['order']
@@ -57,27 +66,30 @@ def cartData(request):
 
     return {'cartItems': cartItems, 'order': order, 'items': items}
 
+# Função para criar um pedido para usuários não autenticados
 def guestOrder(request, data):
-    print('User is not logged in..')
-
-    print('Cookies:', request.COOKIES)
+    # Obtém os dados do formulário enviado pelo cliente
     name = data['form']['name']
     email = data['form']['email']
 
+    # Obtém os dados do carrinho baseado em cookies
     cookieData = cookieCart(request)
     items = cookieData['items']
 
+    # Cria ou obtém um cliente com base no e-mail
     customer, created = Customer.objects.get_or_create(
         email=email,
     )
     customer.name = name
     customer.save()
 
+    # Cria um pedido associado ao cliente
     order = Order.objects.create(
         customer = customer,
         complete = False,
     )
 
+    # Adiciona os itens do carrinho ao pedido
     for item in items:
         product = Product.objects.get(id=item['product']['id'])
         orderItem = OrderItem.objects.create(
--- store/views.py
+++ store/views.py
@@ -10,14 +10,15 @@ from.models import *
 from .utils import cookieCart, cartData, guestOrder
 from .forms import CustomUserCreationForm
 
-
+# View para exibir a página principal da loja
 def store(request):
+    #'Obtém os dados do carrinho   
     data = cartData(request)
-
     cartItems = data['cartItems']
     order = data['order']
     items = data['items']
 
+    # Verifica se o usuário está autenticado e cria um objeto Customer se não existir
     if request.user.is_authenticated:
         Customer.objects.get_or_create(
             user=request.user, 
@@ -27,43 +28,51 @@ def store(request):
             }
         )
 
-
+    #'Obtém todos os produtos da loja
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
+# View para exibir a página do carrinho
 def cart(request):
+    #'Obtém os dados do carrinho
     data = cartData(request)
-
     cartItems = data['cartItems']
     order = data['order']
     items = data['items']
 
+    # Passa os dados do carrinho para o template
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
+# View para exibir a página de checkout
 def checkout(request):
+    #'Obtém os dados do carrinho
     data = cartData(request)
-
     cartItems = data['cartItems']
     order = data['order']
     items = data['items']
 
+    # passa os dados do carrinho para o template
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
+# View para atualizar o item no carrinho
 def updateItem(request):
-    try:
+    try: # obtem os dados enviados pelo cliente
         data = json.loads(request.body)
         productId = data['productId']
         action = data['action']
 
+        # Obtém o cliente e o pedido associado
         customer = request.user.customer
         product = Product.objects.get(id=productId)
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
+        # Obtém ou cria o item do pedido
         orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
+        # Atualiza a quantidade do item com base na ação
         if action == 'add':
             orderItem.quantity += 1
         elif action == 'remove':
@@ -71,9 +80,11 @@ def updateItem(request):
 
         orderItem.save()
 
+        # Remove o item se a quantidade for zero
         if orderItem.quantity <= 0:
             orderItem.delete()
 
+        # Retorna os dados atualizados do carrinho
         cartItems = order.get_cart_items
         cartTotal = round(order.get_cart_total, 2)
         productQuantity = orderItem.quantity if orderItem.quantity > 0 else 0
@@ -81,36 +92,34 @@ def updateItem(request):
 
         return JsonResponse({'cartItems': cartItems, 'cartTotal': cartTotal, 'productQuantity': productQuantity, 'productTotal': productTotal}, safe=False)
     except Exception as e:
-        print('Erro:', e)
         return JsonResponse({'error': str(e)}, status=500)
 
+# View para processar o pedido
 def processOrder(request):
+    # Gera um id único com base no timestamp atual
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
-
+    
     if request.user.is_authenticated:
+        # Obtém o cliente autenticado e o pedido associado
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
     else:
+        # Para usuários não autenticados, cria um pedido temporário
         customer, order = guestOrder(request, data)
 
     # Substituir vírgula por ponto no total enviado
     total = float(data['form']['total'].replace(',', '.'))
     order.transaction_id = transaction_id
 
-    # Logs para depuração
-    print(f"Total enviado pelo cliente: {total}")
-    print(f"Total calculado no servidor: {order.get_cart_total}")
-
-    # Verificar se os totais correspondem
+    # Verifica se o total enviado pelo cliente corresponde ao total calculado no servidor
     if abs(round(total, 2) == round(order.get_cart_total, 2))< 0.01:
         order.complete = True
         order.save()
-        print(f"Pedido {order.id} marcado como completo.")
     else:
-        print('Erro: os totais não correspondem.')
         return JsonResponse({'error': 'Total não corresponde ao valor calculado'}, status=400)
 
+    # Se o pedido requer envio, cria um endereço de envio associado
     if order.shipping:
         ShippingAddress.objects.create(
             customer=customer,
@@ -123,6 +132,17 @@ def processOrder(request):
 
     return JsonResponse('Payment submitted..', safe=False)
 
+# View para exibir os detalhes de um produto específico
+def productDetail(request, pk):
+     # Obtém os dados do carrinho para o usuário atual ou visitante
+    data = cartData(request)
+    cartItems = data['cartItems']
+    # Passa o produto e os itens do carrinho para o template
+    product = Product.objects.get(id=pk)
+    context = {'product': product, 'cartItems': cartItems}
+    return render(request, 'store/product_detail.html', context)
+
+# View para exibir a página de login
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('store')
@@ -139,6 +159,7 @@ def loginPage(request):
 
         return render(request, 'store/login.html')
 
+# View para exibir a página de registro
 def registerPage(request):
     if request.user.is_authenticated:
         return redirect('store')
@@ -160,15 +181,7 @@ def registerPage(request):
         context = {'form': form}
         return render(request, 'store/register.html', context)
 
+# View para fazer logout do usuário
 def logoutUser(request):
     logout(request)
     return redirect('login')
-
-def productDetail(request, pk):
-    data = cartData(request)
-    cartItems = data['cartItems']
-
-    product = Product.objects.get(id=pk)
-    context = {'product': product, 'cartItems': cartItems}
-    return render(request, 'store/product_detail.html', context)
-    
